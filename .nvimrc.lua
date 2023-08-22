b15475b9a4368104d070a85a36a0768b62d84d21
Yaroslav <q.link0.p@gmail.com>
2023-08-22 20:10:33 +0300

    Removed .nvimrc.lua

--- .nvimrc.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command("Dev", function()
-  local current_tab = vim.api.nvim_get_current_tabpage()
-  vim.cmd ":tabnew | terminal pnpm run dev --host"
-  vim.api.nvim_buf_set_name(0, "dev server")
-  vim.api.nvim_set_current_tabpage(current_tab)
-  print "Started dev server in a new tab"
-end, { nargs = 0, desc = "Start a dev server in a new terminal tab" })
